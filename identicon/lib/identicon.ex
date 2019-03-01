797a49a6aa4ac1e5f8e30d2e0c6690c50685be77
Branden <branden@spreedly.com>
2019-03-01 09:27:50 -0500

    upload saved work

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -12,7 +12,48 @@ defmodule Identicon do
       :world
 
   """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+  end
+
+def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+        rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
+end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+        grid = 
+            hex
+            |> Enum.chunk_every(3,3, :discard)
+            |> Enum.map(&mirror_row/1)
+            |> List.flatten
+            |> Enum.with_index
+
+        %Identicon.Image{image | grid: grid}
+    end
+
+  def mirror_row(row) do
+    # [145, 46, 200]
+    [first, second | _tail ] = row
+    # [145, 46, 200, 46, 145]
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r ,g ,b}}
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 end
